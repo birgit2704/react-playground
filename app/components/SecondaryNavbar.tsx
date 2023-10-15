b7b292c3713e55198964dd290585044aa5c58345
Birgit <birgit.bartenschlager@web.de>
2023-10-15 16:42:37 +0200

    images secondary navbar

--- app/components/SecondaryNavbar.tsx
+++ app/components/SecondaryNavbar.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import fisch from "../../public/images/fisch.jpg";
+import hund from "../../public/images/hund.jpg";
 
 const SecondaryNavbar = () => {
   return (
     <nav className="flex justify-around">
       <Link href="/angeln">
-        <Image src="/images/fisch.jpg" alt="" width={50} height={50} />
+        <Image src={fisch} alt="" width={50} height={50} />
         Angeln
       </Link>
       <Link href="/jagen">
-        <Image src="/images/hund.jpg" alt="" width={50} height={50} />
+        <Image src={hund} alt="" width={50} height={50} />
         Jagen
       </Link>
     </nav>
