ca488e00d972c400a3adc4ed87de828dcef06a66
Birgit <birgit.bartenschlager@web.de>
2023-10-16 17:58:34 +0200

    dropdown

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -8,7 +8,24 @@ const Navbar = () => {
       <Link href="/" className="mr-5">
         <Image src="/images/hund.jpg" alt="logo" width={50} height={50} />
       </Link>
+
       <div className="flex gap-2 ml-auto">
+        <div className="dropdown dropdown-hover">
+          <label tabIndex={0} className="bg-base-100 p-3">
+            <Link href="/angeln">Dropdown</Link>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-200  w-52 mt-3"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
         <Link href="/angeln/fuf">
           <Image src="/images/fisch.jpg" alt="" width={50} height={50} />
           Fuf
